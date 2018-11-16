76cfd80f7cd02bc4c1e0865f5ab7cb7a80a5ec58
PhilippBrandy <45015806+PhilippBrandy@users.noreply.github.com>
2018-11-16 15:17:50 +0100

    Arrows can destroy "cuttable"
    cuttable-tag
    particles spawn on destruction of "cuttable"
    --> TODO: texture of rope-pieces as particles

---
+++ Assets/Projectile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public GameObject destroyEffect;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("cuttable"))
+        {
+            other.GetComponent<cuttableEnvi>().health = 0;
+            Instantiate(destroyEffect, transform.position, Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/cuttableEnvi.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cuttableEnvi : MonoBehaviour {
+
+    
+    public int health = 1;
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (health < 1)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+}
