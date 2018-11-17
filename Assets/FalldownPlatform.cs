aee34a284c9de0c3675642d131bd5c9447882452
PhilippBrandy <45015806+PhilippBrandy@users.noreply.github.com>
2018-11-17 20:07:13 +0100

    Respawn-system
    restart scene when no checkpoint was reached
    set location to checkpoint if checkpoint was reached

---
+++ Assets/FalldownPlatform.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FalldownPlatform : MonoBehaviour {
+
+    private bool isHanging = true;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.childCount == 0 && isHanging)
+        {
+            Rigidbody2D myRigidbody = gameObject.AddComponent<Rigidbody2D>(); // Add the rigidbody.
+            myRigidbody.mass = 5;
+            isHanging = false;
+        }
+	}
+}
