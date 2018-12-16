876d1f9688ae5c66c64ee84809ec06ea55eb8efc
PhilippBrandy <45015806+PhilippBrandy@users.noreply.github.com>
2018-12-16 21:08:10 +0100

    Tutorial Level Playable
    Next: parallax

---
+++ Assets/FalldownTree.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FalldownTree : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Rigidbody2D myRigidbody = gameObject.AddComponent<Rigidbody2D>(); // Add the rigidbody.
+        myRigidbody.mass = 5;
+        gameObject.tag = "falling";
+    }
+}
---
+++ Assets/Totem_Controller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Totem_Controller : MonoBehaviour {
+    public GameObject passiveTotem;
+    public GameObject activeTotem;
+    public GameObject lightningFX;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            passiveTotem.SetActive(false);
+            activeTotem.SetActive(true);
+            Instantiate(lightningFX, transform.position, Quaternion.identity);
+        }
+    }
+}
