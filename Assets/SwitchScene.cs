cda823353444b13ae7971fb625e90ab77de36e83
Philipp Brantner <S1710238007@students.fh-hagenberg.at>
2019-01-17 19:49:49 +0100

    .

---
+++ Assets/SwitchScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour
+{
+    public string nextScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+    // Update is called once per frame
+
+}
