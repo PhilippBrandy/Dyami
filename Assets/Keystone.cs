0758b5002e85a1280300e0596d3fc8dce60bd712
Philipp Brantner <S1710238007@students.fh-hagenberg.at>
2019-01-20 15:49:27 +0100

    Keystone

---
+++ Assets/Keystone.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keystone : MonoBehaviour
+{
+    public GameObject activeEyes;
+    public GameObject notactiveEyes;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (other.GetComponentInChildren<ShootArrow>().theForce == true)
+            {
+                activeEyes.SetActive(true);
+                notactiveEyes.SetActive(false);
+            }
+        }
+    }
+}
