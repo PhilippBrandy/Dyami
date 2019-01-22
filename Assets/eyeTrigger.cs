273bc25e052e06134a4ca52b554d9cbc4926cd21
peanutboy <44230552+peanutboy@users.noreply.github.com>
2019-01-22 12:18:16 +0100

    eye stone edited

--- Assets/eyeTrigger.cs
+++ Assets/eyeTrigger.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class eyeTrigger : MonoBehaviour
 {
+    public GameObject eyeGlow;
     public GameObject eyehandler;
+    public Animation eyeOpenening;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("eye trigger set active(false)");
         eyehandler.SetActive(false);
+        eyeGlow.SetActive(false);
     }
     
 
@@ -19,8 +22,14 @@ public class eyeTrigger : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             Debug.Log("collider player");
-
+            eyeOpenening.Play();
             eyehandler.SetActive(true);
+            eyeGlow.SetActive(true);
         }
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        eyehandler.SetActive(false);
+        eyeGlow.SetActive(false);
     }
 }
