ee9eeb0fd0496c5c2b570a3b62aa1a1f96146290
peanutboy <44230552+peanutboy@users.noreply.github.com>
2019-01-23 22:31:00 +0100

    feather anim and UIText implemented for tutorial

--- Assets/ActivateAbility.cs
+++ Assets/ActivateAbility.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ActivateAbility : MonoBehaviour
 {
     public ShootArrow characterWeapon;
+   public GameObject feather;
+    public Animator featherAnim;
+    private bool featherpower;
+    public GameObject textObject;
+  
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        feather.SetActive(true);
+        characterWeapon.learnedTeleporting = false;
+        featherpower = true;
+        textObject.SetActive(false);
 
-    // Update is called once per frame
-    void Update()
+}
+
+// Update is called once per frame
+void Update()
     {
         
     }
@@ -20,7 +30,22 @@ public class ActivateAbility : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            
             characterWeapon.learnedTeleporting = true;
+             if (featherpower == true)
+        {
+            featherAnim.SetTrigger("Start");
+
         }
+
+        }
+       
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        feather.SetActive(false);
+        featherpower = false;
+        textObject.SetActive(true);
+        // Destroy(feather);
     }
 }
