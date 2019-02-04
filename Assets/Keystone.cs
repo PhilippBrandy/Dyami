d39eb4442225387ca695fc806a03604c406e6d92
PhilippBrandy <45015806+PhilippBrandy@users.noreply.github.com>
2019-02-04 16:49:04 +0100

    rework Portal activation

--- Assets/Keystone.cs
+++ Assets/Keystone.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 
 public class Keystone : MonoBehaviour
 {
+    public RiddleManager owner;
     public GameObject activeEyes;
     public GameObject notactiveEyes;
+    public bool activated;
+    public string key;
+
+    private void Start()
+    {
+        activated = false;
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && activated == false)
         {
             if (other.GetComponentInChildren<ShootArrow>().theForce == true)
             {
                 activeEyes.SetActive(true);
                 notactiveEyes.SetActive(false);
+                activated = true;
+                owner.collectedPassword += key;
             }
         }
+    }
+
+    public void deactivate()
+    {
+        activeEyes.SetActive(false);
+        notactiveEyes.SetActive(true);
+        activated = false;
     }
 }
---
+++ Assets/RiddleManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RiddleManager : MonoBehaviour
+{
+    public Keystone key1;
+    public Keystone key2;
+    public Keystone key3;
+    public string password;
+    public string collectedPassword;
+    public GameObject toActivate;
+    public GameObject toDeactivate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (collectedPassword.Length == 3)
+        {
+            if (collectedPassword == password)
+            {
+                toActivate.SetActive(true);
+                toDeactivate.SetActive(false);
+            }
+            else
+            {
+                deactivateAll();
+            }
+        }
+    }
+
+
+    private void deactivateAll()
+    {
+        key1.deactivate();
+        key2.deactivate();
+        key3.deactivate();
+        password = "";
+    }
+}
