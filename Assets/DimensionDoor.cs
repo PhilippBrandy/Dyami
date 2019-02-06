6cbb2b8f15bbd565b76622cf583863c0511ca1e0
PhilippBrandy <45015806+PhilippBrandy@users.noreply.github.com>
2019-02-06 11:06:28 +0100

    Teleport to "Endscreen"

---
+++ Assets/DimensionDoor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DimensionDoor : MonoBehaviour
+{
+    public Transform exit;
+    public Transform player;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            player.position = exit.position + Vector3.right;
+        }
+    }
+}
