355497d218956a69cfe9d9183ead4747e82f6148
peanutboy <44230552+peanutboy@users.noreply.github.com>
2019-04-29 10:07:57 +0200

    visuel update cave platforms + simple script to change splineindex

---
+++ Assets/testScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.U2D;
+
+
+public class testScript : MonoBehaviour
+{
+
+    public SpriteShapeController spriteShapeController;
+    public int index;
+    
+  
+   
+    private Spline spline;
+    private int lastSpritePointCount;
+
+    void Awake()
+    {
+        Debug.Log("awake");
+
+        spline = spriteShapeController.spline;
+    }
+    private void Update()
+    {
+        Debug.Log("update");
+        spline.SetSpriteIndex(1, 1);
+        Invoke("changeIndex1", 1f);
+    }
+
+     void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("trigger enter");
+
+            spline.SetSpriteIndex(1, 1);
+            Invoke("changeIndex", 1f);
+        }
+
+    }
+
+    public void changeIndex()
+    {
+        Debug.Log("change index");
+
+        spline.SetSpriteIndex(1, 1);
+        Invoke("changeIndex1", 1f);
+    }
+    public void changeIndex1()
+    {
+        spline.SetSpriteIndex(1, 0);
+        Invoke("changeIndex", 1f);
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        
+        
+
+    }
+
+}
