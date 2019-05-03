e2a5c4ff647df85c25891490872d9d4067346638
Katrin <ktrntbr@gmx.at>
2019-05-03 15:22:43 +0200

    Instantiating throwable objects
    time of spawning, spawning delay, current path and spawnpoint customizable in editor

---
+++ Assets/Scripts/ThrowObjects/InstThrowableObjects.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstThrowableObjects : MonoBehaviour
+{
+    public GameObject prefab;
+    public GameObject curObject;
+    public Transform spawnpoint;
+    public float seconds = 5.0f;
+    public float startTime = 3.0f;
+
+    // to stop throwing by another script
+    public bool stopSpawning = false;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnObjects", startTime, seconds);
+    }
+
+    public void SpawnObjects()
+    {
+        curObject = Instantiate(prefab, spawnpoint.transform.position, Quaternion.identity);
+        if (stopSpawning)
+        {
+            // stop throwing the objects
+        }
+    }
+}
--- Assets/Scripts/ThrowObjects/MoveOnPathScript.cs
+++ Assets/Scripts/ThrowObjects/MoveOnPathScript.cs
@@ -4,25 +4,29 @@ using UnityEngine;
 
 public class MoveOnPathScript : MonoBehaviour
 {
-    public EditorPathScript PathToFollow;
+    EditorPathScript PathToFollow;
     public int curWayPoint = 0;
     public float speed;
     // the distance between the object and the cur waypoint
     public float reachDistance = 1.0f;
     public float rotationSpeed = 5.0f;
-    public string pathName;
+
     Vector3 lastPosition;
     Vector3 curPosition;
+
+    public string pathName = "PathHolder";
+    public string instantiatingObject = "InstantiateObjectHolder";
 
     void Start()
     {
         // for random paths
-        //PathToFollow = GameObject.Find(pathName).GetComponent<EditorPathScript>();
+        PathToFollow = GameObject.Find(pathName).GetComponent<EditorPathScript>();
         lastPosition = transform.position;
     }
 
     void Update()
     {
+
         float distance = Vector3.Distance(PathToFollow.pathObjects[curWayPoint].position, transform.position);
         transform.position = Vector3.Lerp(transform.position, PathToFollow.pathObjects[curWayPoint].position, Time.deltaTime * speed);
         // or MoveTowards
@@ -35,7 +39,6 @@ public class MoveOnPathScript : MonoBehaviour
             transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         }
 
- 
         //Vector3 relativePos = PathToFollow.pathObjects[curWayPoint].position - transform.position;
         //Quaternion LookAtRotation = Quaternion.LookRotation(relativePos);
         //Quaternion LookAtRotationOnly_Y = Quaternion.Euler(transform.rotation.eulerAngles.x, LookAtRotation.eulerAngles.y, transform.rotation.eulerAngles.z);
@@ -53,8 +56,11 @@ public class MoveOnPathScript : MonoBehaviour
 
         if (curWayPoint >= PathToFollow.pathObjects.Count)
         {
-            // or destroy the object here
-            curWayPoint = 0;
+            // destroy the object here
+            Destroy(this.gameObject);
+
+            // or start the cycle
+            //curWayPoint = 0;
         }
     }
 }
