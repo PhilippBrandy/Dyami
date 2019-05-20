962eba41a12b8b02bed4b19410f7c3d3831276d1
peanutboy <44230552+peanutboy@users.noreply.github.com>
2019-05-20 19:40:32 +0200

    rising water area playable (reset works)

--- Assets/waterRising.cs
+++ Assets/waterRising.cs
@@ -5,19 +5,13 @@ using UnityEngine;
 public class waterRising : MonoBehaviour
 {
     public Animator animatorWater;
+    public Animator TreesReset;
     public GameObject waterfall;
     public string triggerName;
     // triggerNames:
     //"Dashed" water starts to rise
     //"reset" water set to default position after rising
-    //
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-        
-    }
     // only if the player revives at checkpoint
     void OnTriggerStay2D(Collider2D collision)
     {
@@ -25,25 +19,22 @@ public class waterRising : MonoBehaviour
         //nur wenn der spieler beim savepoint wiederbelebt wird soll triggeranimation mit reset akiviert werden
         if (collision.CompareTag("Player")&& triggerName=="reset")
         {
-        triggerAnimation();
-           
-        }
-        
-        
+            Debug.Log("enter:" + collision.name);
+           // animatorWater.StopPlayback();
+            triggerAnimation(); 
+        }  
     }
-
     // Update is called once per frame
     public void triggerAnimation()
     {
         Debug.Log("triggerAnimation:"+triggerName);
-       
         animatorWater.SetTrigger(triggerName);
         waterfall.SetActive (false);
-        //wenn der spieler beimsavepoint wiederbelebt wird soll der waterfall wieder resetet werden
+        //wenn der spieler beimsavepoint wiederbelebt wird soll der waterfall und der tree wieder resetet werden
         if (triggerName == "reset")
         {
             waterfall.SetActive(true);
-
+            TreesReset.SetTrigger("reset");
         }
     }
 }
