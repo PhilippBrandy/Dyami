3b9574bca1f53b2dd9b88c4906138aba1892a2ea
PhilippBrandy <45015806+PhilippBrandy@users.noreply.github.com>
2019-06-04 15:27:25 +0200

    beaver'scave finished
    TODO: combine beavers-cave with rest of the level
    background

---
+++ Assets/floatingHead.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class floatingHead : MonoBehaviour
+{
+    Animation floatinganimation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        floatinganimation = gameObject.GetComponent<Animation>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!floatinganimation.isPlaying)
+        {
+            floatinganimation.Play();
+        }
+    }
+}
