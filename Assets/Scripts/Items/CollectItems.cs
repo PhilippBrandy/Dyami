c847bd630f46930c153d9763df36551ac54262f2
Katrin <ktrntbr@gmx.at>
2019-06-08 13:25:17 +0200

    Items collectable
    Zum testen in der Tutorial+lvl1-Szene unter --ITEMS--. Avatar mit neuem CollectItems-Script: Hier kann man die prefabs ins Items-Array reinziehen, sowie den Items-Canvas mit dem dazugehörigen Text, der sich nach itemstatus anpasst. Der ItemsCanvas soll vor spielbeginn inaktiv sein, wird erst aktiviert, wenn der spieler ein item einsammelt. Das Prefab muss einen boxCollider mit trigger haben, und das script Item tragen. Der TriggerToStopItemCanvas blendet den Canvas wider aus und setzt die gesammelten items wieder zurück (mit dem script ResetItemCanvas).

---
+++ Assets/Scripts/Items/CollectItems.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CollectItems : MonoBehaviour
+{
+
+    public GameObject[] items;
+    int collectedItems = 0;
+    public TMPro.TextMeshProUGUI itemText;
+    public GameObject itemsCanvas;
+    int itemsNumber;
+    int curItemNumber = 0;
+    bool showItemsText = false;
+
+
+    void Start()
+    {
+        itemsNumber = items.Length;
+    }
+
+    void Update()
+    {
+        if (showItemsText)
+        {
+            itemsCanvas.SetActive(true);
+        }
+
+        if (curItemNumber == itemsNumber)
+        {
+            // all items collected
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("CollectableItem"))
+        {
+            curItemNumber++;
+            showItemsText = true;
+            itemText.text = curItemNumber.ToString() + " / " + itemsNumber.ToString();
+            other.gameObject.SetActive(false);
+        }
+    }
+
+
+    public int GetItemsNumber()
+    {
+        return items.Length;
+    }
+
+    public int GetItemsCollected()
+    {
+        return collectedItems;
+    }
+
+    public void SetItemsCollectedNumber(int newNumber)
+    {
+        curItemNumber = newNumber;
+    }
+
+    public void SetItemsCanvasInvisible()
+    {
+        showItemsText = false;
+        itemsCanvas.SetActive(false);
+    }
+        
+}
