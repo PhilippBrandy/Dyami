0075db6b553db8f82a92eebc23dc18583e4c438a
peanutboy <44230552+peanutboy@users.noreply.github.com>
2019-06-22 00:49:57 +0200

    update endboss

---
+++ Assets/IgnoreCollision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreCollision : MonoBehaviour
+{
+    public Transform bulletPrefab;
+
+    void Start()
+    {
+        Transform bullet = Instantiate(bulletPrefab) as Transform;
+        Physics2D.IgnoreCollision(bullet.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+    }
+}
