3237e2281911458c6e0eb484dea339ce25cc8a81
PhilippBrandy <45015806+PhilippBrandy@users.noreply.github.com>
2019-06-24 10:04:59 +0200

    reset Platform script implemented
    use on the pivot-object
    and then add the character to the killable-slot

---
+++ Assets/ResetPlatform.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPlatform : MonoBehaviour
+{
+    public GameObject platform;
+    private Transform plTrans;
+    private SpriteRenderer plRend;
+    private FalldownPlatform plFallPlat;
+    private GameObject clone;
+
+    public Killable player;
+    private bool respawned;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CopyCurrent();
+        respawned = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.health <= 0 && !respawned)
+        {
+            activateNewAndDeleteOld();
+        }
+        if (respawned && player.health > 0)
+        {
+            respawned = false;
+        }
+    }
+
+    void CopyCurrent()
+    {
+        clone = Instantiate(platform, gameObject.transform, false);
+        clone.SetActive(false);
+    }
+
+    void activateNewAndDeleteOld()
+    {
+        platform.SetActive(false);
+        platform = clone;
+        platform.SetActive(true);
+        CopyCurrent();
+        respawned = true;
+    }
+
+}
