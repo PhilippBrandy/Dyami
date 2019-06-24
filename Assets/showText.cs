db5ef20c73dfc154213b0cd4e970952cb3f2cf98
peanutboy <44230552+peanutboy@users.noreply.github.com>
2019-06-24 19:05:17 +0200

    update new textboxes in rework

---
+++ Assets/showText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showText : MonoBehaviour
+{
+    public GameObject obj;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("text trigger");
+        if (collision.CompareTag("Player"))
+        {
+            obj.SetActive(true);
+
+        }
+        
+    }
+    void OnTriggerExit2D(Collider2D collision)
+        {
+        if (collision.CompareTag("Player"))
+        {
+            obj.SetActive(false);
+
+
+        }
+
+    }
+}
