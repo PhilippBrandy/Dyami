c98732e20e4c25d54bd0e218a8744ccda942a882
peanutboy <44230552+peanutboy@users.noreply.github.com>
2019-06-24 18:48:30 +0200

    endboss scene reload added

---
+++ Assets/sceneReload.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class sceneReload : MonoBehaviour
+{
+    public GameObject avatar;
+    public Scene scene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (avatar.GetComponent<Killable>().health == 0)
+        {
+        scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+
+        }
+
+    }
+}
