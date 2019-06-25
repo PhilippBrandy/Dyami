cef3d4e5fc5ff5c509f81d712587244f63aa48fd
Katrin <ktrntbr@gmx.at>
2019-06-25 12:46:12 +0200

    Update SetLoadedGame.cs

--- Assets/Scripts/SaveGameScripts/SetLoadedGame.cs
+++ Assets/Scripts/SaveGameScripts/SetLoadedGame.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SetLoadedGame : MonoBehaviour
 {
     public GameObject player;
+    public string nameOfSecondLevel = "wood level blocking";
 
     private void Update()
     {
@@ -12,6 +14,11 @@ public class SetLoadedGame : MonoBehaviour
         {
             player.transform.position = GameManager.instance.GetPlayerPos();
             player.GetComponent<ShootArrow>().learnedTeleporting = GameManager.instance.CanPlayerTeleport();
+
+            if (SceneManager.GetActiveScene().name.Contains(nameOfSecondLevel))
+            {
+                player.GetComponent<ShootArrow>().learnedTeleporting = true;
+            }
             GameManager.instance.SetIsGameLoaded(false);
         }
     }
