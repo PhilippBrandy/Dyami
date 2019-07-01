078cd0ea0ad4b9ede80cdea0557474740586106a
peanutboy <44230552+peanutboy@users.noreply.github.com>
2019-07-01 22:24:07 +0200

    updates spirit ocean

---
+++ Assets/setActiveObj.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setActiveObj : MonoBehaviour
+{
+    public GameObject[] ObjectsetActive;
+    public GameObject[] ObjectsetPassive;
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") )
+        {
+            for(int i=0; i< ObjectsetActive.Length; i++)
+            {
+            ObjectsetPassive[i].SetActive(false);
+
+            }
+            for (int i = 0; i < ObjectsetActive.Length; i++)
+            {
+                ObjectsetActive[i].SetActive(true);
+
+            }
+        }
+
+    }
+}
